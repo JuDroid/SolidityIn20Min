fa2d2aca7533a72fefb029d2e719f51d3f24bca4
JuDroid <juanmiguelquiroz4@gmail.com>
2023-07-14 13:41:00 -0500

    practica de sintaxis basica de Solidity

---
+++ SolidityIn20Min/soliditySyntax.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.20; //Especificamos la version de Solidity
+
+
+contract ramdomContract { //creamos nuestro contrato 
+
+    // Dentro de las llaves de nuestro contrato escribimos
+    //el codigo del contrato
+
+}
+
+
